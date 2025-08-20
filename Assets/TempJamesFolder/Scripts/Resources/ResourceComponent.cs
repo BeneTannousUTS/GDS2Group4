1396a1532f58ee78571f6150d276ee087ea5c506
JamesK-XIV <jameskidman14@gmail.com>
2025-08-20 14:23:07 +1000

    Created temp folders and created base resource component script

---
+++ Assets/TempJamesFolder/Scripts/Resources/ResourceComponent.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceComponent : MonoBehaviour
+{
+    private int ResourceID;
+    private string ResourceName;
+    private float ResourceWeight;
+    private int ResourceRarity;
+    private Image ResourceIcon;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
