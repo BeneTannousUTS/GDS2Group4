16a67a1332d328d9a4e4b7bb01d84e720106d949
Mesmaa <60990360+Julianm-25@users.noreply.github.com>
2025-08-20 16:52:30 +1000

    As a player, I want to be able to control my character's movement so that I can move my character around the game and explore.

--- Assets/Assets/Scripts/Player/PlayerController.cs
+++ Assets/Assets/Scripts/Player/PlayerController.cs
@@ -1,29 +1,51 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
 
-    float cameraSensitivity = 400f;
+    public float cameraSensitivity = 10f, moveSpeed = 5f;
     float upperLookLimit = 89f, lowerLookLimit = -89f;
     Transform camObject;
     private bool isCamLocked = false;
     private float vertical, horizontal, currentCamY = 0f;
+    private Vector3 moveVector;
+    private CharacterController characterController;
+    InputAction lookAction, moveAction, interactAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        characterController = GetComponent<CharacterController>();
+        lookAction = InputSystem.actions.FindAction("Look");
+        moveAction = InputSystem.actions.FindAction("Move");
+        interactAction = InputSystem.actions.FindAction("Interact");
         camObject = GetComponentInChildren<Camera>().transform;
-        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        horizontal = Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
-        vertical = Input.GetAxis("Mouse Y") * -cameraSensitivity * Time.deltaTime;
+        GetInputs();
+        HandleMovement();
     }
 
-    // Update is called once per frame
     void LateUpdate()
+    {
+        MouseLook();
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    public void ToggleCamLock()
+    {
+        isCamLocked = !isCamLocked;
+    }
+
+    private void MouseLook()
     {
         if (!isCamLocked)
         {
@@ -35,8 +57,21 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    public void ToggleCamLock()
+    private void HandleMovement()
     {
-        isCamLocked = !isCamLocked;
+        moveVector.Normalize();
+        if (!characterController.isGrounded)
+        {
+            moveVector.y = -2;
+        }
+        moveVector = transform.TransformDirection(moveVector);
+        characterController.Move(moveVector * Time.deltaTime * moveSpeed);
+    }
+
+    private void GetInputs()
+    {
+        horizontal = lookAction.ReadValue<Vector2>().x * cameraSensitivity * Time.fixedDeltaTime;
+        vertical = lookAction.ReadValue<Vector2>().y * -cameraSensitivity * Time.fixedDeltaTime;
+        moveVector = new Vector3(moveAction.ReadValue<Vector2>().x, 0, moveAction.ReadValue<Vector2>().y);
     }
 }
