be69fa82067ac2e96f878e55f61720c254a49f72
JamesK-XIV <jameskidman14@gmail.com>
2025-08-20 14:44:47 +1000

    created temp resources, finished task resource system

--- Assets/TempJamesFolder/Scripts/Resources/ResourceComponent.cs
+++ Assets/TempJamesFolder/Scripts/Resources/ResourceComponent.cs
@@ -3,11 +3,18 @@ using UnityEngine.UI;
 
 public class ResourceComponent : MonoBehaviour
 {
+    [SerializeField]
     private int ResourceID;
+    [SerializeField]
     private string ResourceName;
+    [SerializeField]
     private float ResourceWeight;
+    [SerializeField]
     private int ResourceRarity;
+    [SerializeField]
     private Image ResourceIcon;
+    [SerializeField]
+    private bool ShipPart;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
