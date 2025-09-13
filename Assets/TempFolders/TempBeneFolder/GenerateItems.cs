d199a2464ebddfde9282a90483480f4c09f22500
MoosemanEX <datboi.mooseman06@gmail.com>
2025-09-14 03:54:00 +1000

    Added script to spawn resources around the map with weighting based on biome

--- Assets/TempFolders/TempBeneFolder/GenerateItems.cs
+++ Assets/TempFolders/TempBeneFolder/GenerateItems.cs
@@ -1,32 +1,33 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GenerateItems : MonoBehaviour
 {
     [Header("Assets to Scatter")]
     public GameObject[] allResources;
-    public float objectsPerSquareMeter = 0.005f;
-    
+    public float objectDensity = 5f;
+
     [Header("Forest Biome Resources")]
     public GameObject[] forestBiomeResources;
-    
+
     [Header("Factory Biome Resources")]
     public GameObject[] factoryBiomeResources;
-    
+
     [Header("Mountains Biome Resources")]
     public GameObject[] mountainBiomeResources;
-    
-    private float maxSlope = 40;
 
+    private float maxSlope = 45;
     private Terrain terrain;
-    void Start()
-    {
-    }
 
     void Awake()
     {
         terrain = GetComponent<Terrain>();
+        if (terrain != null)
+        {
+            ScatterObjects();
+        }
     }
-    
+
     public void ScatterObjects()
     {
         TerrainData terrainData = terrain.terrainData;
@@ -34,41 +35,79 @@ public class GenerateItems : MonoBehaviour
         Vector3 tilePos = terrain.transform.position;
 
         float area = terrainSize.x * terrainSize.z;
-        int numberOfObjects = Mathf.RoundToInt(area * objectsPerSquareMeter);
+        int numberOfObjects = Mathf.RoundToInt(area * (objectDensity / 1000));
 
         for (int i = 0; i < numberOfObjects; i++)
         {
             float localX = Random.Range(0, terrainSize.x);
             float localZ = Random.Range(0, terrainSize.z);
 
-            Vector3 worldPos = new Vector3(tilePos.x + localX, 0, tilePos.z + localZ);
+            Vector3 rayStart = new Vector3(tilePos.x + localX, tilePos.y + terrainSize.y + 50f, tilePos.z + localZ);
 
-            float terrainHeight = terrain.SampleHeight(worldPos);
-            Vector3 normal = terrainData.GetInterpolatedNormal(
-                localX / terrainSize.x,
-                localZ / terrainSize.z
-            );
-
-            // Slope check
-            float slope = Vector3.Angle(Vector3.up, normal);
-            if (slope > maxSlope)
-                continue; // Skip this spot if too steep
-
-            GameObject prefab = allResources[Random.Range(0, allResources.Length)]; //TODO: Replace this with logic to get terrain layer and spawn relevant resource
-            GameObject instance = Instantiate(prefab, transform, true);
-
-            Renderer rend = instance.GetComponentInChildren<Renderer>();
-            float pivotOffset = 0f;
-            if (rend != null)
+            if (Physics.Raycast(rayStart, Vector3.down, out RaycastHit hit, Mathf.Infinity))
             {
-                pivotOffset = rend.bounds.extents.y - (rend.bounds.max.y - rend.bounds.min.y) / 2f;
+                if (!hit.collider.transform.IsChildOf(terrain.transform) && !hit.collider.CompareTag("Tree"))
+                    continue;
+
+                float slope = Vector3.Angle(Vector3.up, hit.normal);
+                if (slope > maxSlope)
+                    continue;
+
+                // Build weighted list based on hit
+                List<GameObject> weightedList = new List<GameObject>();
+
+                // Always add all resources once (equal base chance)
+                weightedList.AddRange(allResources);
+
+                // Boost forest resources if hit terrain layer 1 (forest)
+                if (IsTerrainTexture(hit.point, 1))
+                {
+                    for (int w = 0; w < 3; w++) // weight multiplier
+                        weightedList.AddRange(forestBiomeResources);
+                }
+
+                // Boost factory resources if hit terrain layer 2 (factory)
+                if (IsTerrainTexture(hit.point, 2))
+                {
+                    for (int w = 0; w < 3; w++)
+                        weightedList.AddRange(factoryBiomeResources);
+                }
+
+                // Boost mountain resources if hit terrain layer 3 (mountain)
+                if (IsTerrainTexture(hit.point, 3))
+                {
+                    for (int w = 0; w < 3; w++)
+                        weightedList.AddRange(mountainBiomeResources);
+                }
+
+                // Pick random from weighted list
+                GameObject prefab = weightedList[Random.Range(0, weightedList.Count)];
+                GameObject instance = Instantiate(prefab, transform, true);
+
+                Renderer rend = instance.GetComponentInChildren<Renderer>();
+                float pivotOffset = 0f;
+                if (rend != null)
+                {
+                    pivotOffset = rend.bounds.extents.y - (rend.bounds.max.y - rend.bounds.min.y) / 2f;
+                }
+
+                instance.transform.position = hit.point + Vector3.up * (pivotOffset + 0.5f);
+                instance.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                instance.transform.Rotate(0f, Random.Range(0f, 360f), 0f, Space.Self);
             }
-
-            Vector3 position = new Vector3(localX, terrainHeight + pivotOffset + 0.5f, localZ) + tilePos;
-            instance.transform.position = position;
-
-            instance.transform.rotation = Quaternion.FromToRotation(Vector3.up, normal);
-            instance.transform.Rotate(0f, Random.Range(0f, 360f), 0f, Space.Self);
         }
+    }
+
+    // Check dominant terrain texture at a world position
+    private bool IsTerrainTexture(Vector3 worldPos, int textureIndex)
+    {
+        Vector3 terrainPos = worldPos - terrain.transform.position;
+        TerrainData terrainData = terrain.terrainData;
+
+        int mapX = Mathf.RoundToInt((terrainPos.x / terrainData.size.x) * terrainData.alphamapWidth);
+        int mapZ = Mathf.RoundToInt((terrainPos.z / terrainData.size.z) * terrainData.alphamapHeight);
+
+        float[,,] splatmapData = terrainData.GetAlphamaps(mapX, mapZ, 1, 1);
+        return splatmapData[0, 0, textureIndex] > 0.5f; // dominant texture
     }
 }
